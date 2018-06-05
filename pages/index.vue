af4ec8e73c7eabbcb30e61f5f943ae5b5e392965
paul <khjun325@naver.com>
2018-06-05 12:46:46 +0900

    Portfolio_first_20180605

--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,8 @@
       </div>
     </div>
     <article class="message">
-      <div class="message-header">개란 무엇인가?</div>
-      <div class="message-body">개는 늑대처럼 후각과 청각이 뛰어나고 민첩하며 턱이 강하다. 이러한 장점과 더불어 개는 인간에 대한 충성심이 강하기 때문에 쉽게 훈련 가능한 동물이며, 사회의 많은 분야에서 인간에게 많은 도움을 주고 있는
-        동물이다.
+      <div class="message-header">김형준</div>
+      <div class="message-body">커피를 좋아함
       </div>
     </article>
     <article class="columns">
@@ -39,7 +38,7 @@
       </div>
     </article>
     <article class="box">
-      <h1 class="title"> 보고싶은 개의 정보를 누르세요</h1>
+      <h1 class="title"> 자기소개서</h1>
 
     </article>
   </section>
